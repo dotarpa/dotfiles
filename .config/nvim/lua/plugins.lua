a27e2923c3651d504124bd2d35f2afd3f87a245b
dotarpa <84436393+dotarpa@users.noreply.github.com>
2024-06-01 09:32:03 +0900

    install Indent Blankline

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -11,10 +11,9 @@ return {
     },
     'neovim/nvim-lspconfig',
     'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer', -- buffer source
-    -- 'hrsh7th/cmp-path',
-    -- 'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-buffer',
     'hrsh7th/nvim-cmp',
     'onsails/lspkind.nvim',
+    'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {},
 }
 
